43b37ead677df23d5d145365e7306b2f52eec17d
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-01-30 22:15:04 +0530

    changed phonenumber string to number

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   userName: { type: String, required: true },
-  phoneNumber: { type: String, required: true },
-
+  phoneNumber: { type: Number, required: true },
   password: { type: String, required: true },
 });
 

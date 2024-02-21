07df6d5f784e88ca4c0675a905d6160829d576c2
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-02-21 22:29:07 +0530

    added otp 30sec

--- server/models/otpModel.js
+++ server/models/otpModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const otpSchema = new mongoose.Schema({
   email: { type: String, required: true },
   otp: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now, expires: 60 }, // OTP expires after 1 minutes (adjust as needed)
+  createdAt: { type: Date, default: Date.now, expires: 30 }, // OTP expires after 1 minutes (adjust as needed)
 });
 
 const OTP = mongoose.model("OTP", otpSchema);

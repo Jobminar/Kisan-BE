2be1d2c0d29334bbc72b1e4f7bdc23aef3f99e8c
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-03-01 12:45:24 +0530

    added getConatct

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -31,7 +31,7 @@ const contactController = {
   getAllContacts: async (req, res) => {
     try {
       const contacts = await Contact.find();
-      res.status(200).json(contacts);
+      res.status(200).json({ contacts });
     } catch (error) {
       console.error("Error fetching contacts:", error);
       res.status(500).json({ message: "Something went wrong" });
